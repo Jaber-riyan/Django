fbf4a1cd82a5b46789904c4f7064b26c02268638
Jaber Riyan <129547732+Jaber-riyan@users.noreply.github.com>
2023-11-30 00:40:15 +0600

    Delete practice day 1 week 3/practice_day_1_week_3 directory

--- practice day 1 week 3/practice_day_1_week_3/Practice_1_week_3/Practice_1_week_3/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home_main(request):
-    return render(request,'home_main.html')
--- practice day 1 week 3/practice_day_1_week_3/Simple_Project/first_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def about(request):
-    return render(request, 'first_app/about.html')
-
-def contact(request):
-    return render(request, 'first_app/contact.html')
